64749d758087257f272725e95b87b0c5eae4af73
David Laban <alsuren@gmail.com>
2022-10-12 15:58:35 +0100

    follow bindgen tutorial, with some cmake thrown in

---
+++ support/rust/trax-sys/src/wrapper.h
@@ -0,0 +1,3 @@
+// FIXME: I think that this could probably take the version that cmake puts into
+// $OUT_DIR if we set up our #include path correctly in build.rs
+#include "../../../../include/trax.h"
